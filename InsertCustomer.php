8164d048b93843653a7a42e8c0e14d1f6cc37f07
Jake Griffin <jgriffinmail1@gmail.com>
2022-11-30 15:31:49 -0500

    checkout

--- InsertCustomer.php
+++ InsertCustomer.php
@@ -1,6 +1,6 @@
 <?php
 
-$ID = $_POST['ID'];
+$ID = rand(100000,999999);
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
@@ -13,7 +13,8 @@ $card_num = $_POST['card_num'];
 $Exp_month = $_POST['Exp_month'];
 $Exp_year = $_POST['Exp_year'];
 $cvv = $_POST['cvv'];
-    
+
+
 
 $servername = "localhost";
 $username = "jsphardw_admin";
@@ -22,33 +23,49 @@ $dbname = "jsphardw_idkwhattoputhere";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+$stmt = $conn->prepare("INSERT INTO Customer (Customer_ID , Customer_Name , Customer_Email, Customer_Phone, Customer_Address, Customer_City, Customer_Zip, Customer_State, Card_Name, Credit_Card_Number, Exp_Month, Exp_Year, CVV) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+
+$stmt->bind_param("isssssisssiii", $ID, $name ,$email ,$phone,$addr ,$city ,$zip ,$state,$card_name ,$card_num ,$Exp_month, $Exp_year ,$cvv );
+
+try {  
+        $stmt->execute();  
+        echo "success";
+        echo "<br>";
+        echo $ID;
+        echo "<br>";
+        echo $name;
+        echo "<br>";
+        echo $email ;
+        echo "<br>";
+        echo $phone;
+        echo "<br>";
+        echo $addr ;
+        echo "<br>";
+        echo $city;
+        echo "<br>";
+        echo $zip ;
+        echo "<br>";
+        echo $state ;
+        echo "<br>";
+        echo $card_name ;
+        echo "<br>";
+        echo $card_num ;
+        echo "<br>";
+        echo $Exp_month;
+        echo "<br>";
+        echo $Exp_year; 
+        echo "<br>";
+        echo $cvv;
+
+
+    }  
+catch (Exception $e) {  
+        echo $e;
+    }  
+
+
+$stmt->close();
+$conn->close();
+
 ?>
 
-<!doctype html>
-<html>
-    <head>
-        <title>PHP amd MySQL</title>
-    </head>
-    <body>
-
-        <h1>PHP and MySQL</h1>
-
-        <?php
-
-            $stmt = $conn->prepare("INSERT INTO Customer (Customer_ID , Customer_Name , Customer_Email, Customer_Phone, Customer_Address, Customer_City, Customer_Zip, Customer_State, Card_Name, Credit_Card_Number, Exp_Month, Exp_Year, CVV) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)");
-
-            $stmt->bind_param("i,s,s,s,s,s,i,s,s,s,i,i,i", $ID, $name ,$email ,$phone,$addr ,$city ,$zip ,$state,$card_name ,$card_num ,$Exp_month, $Exp_year ,$cvv );
-
-            if ($stmt->execute()){
-                echo "Thank you for your order";
-            }
-            else{
-                echo"ERROR";
-            }
-
-            $stmt->close();
-            $conn->close();
-
-        ?>
-    </body>
-</html>
