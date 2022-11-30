cdb81f62bd09688aecfd22797a3925633758f21a
Jake Griffin <jgriffinmail1@gmail.com>
2022-11-30 13:31:41 -0500

    checkout

--- checkout.php
+++ checkout.php
@@ -1,3 +1,41 @@
+<?php
+
+if(isset($_POST['submit'])){
+
+  $ID = $_POST['ID'];
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $addr = $_POST['addr'];
+  $city = $_POST['city'];
+  $zip = $_POST['zip'];
+  $state = $_POST['state'];
+  $card_name = $_POST['card_name'];
+  $card_num = $_POST['card_num'];
+  $Exp_month = $_POST['Exp_month'];
+  $Exp_year = $_POST['Exp_year'];
+  $cvv = $_POST['cvv'];
+
+
+  $servername = "localhost";
+  $username = "jsphardw_admin";
+  $password = "pz-;Ry,ePd%W";
+  $dbname = "jsphardw_idkwhattoputhere";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  $stmt = $conn->prepare("INSERT INTO Customer (Customer_ID , Customer_Name , Customer_Email, Customer_Phone, Customer_Address, Customer_City, Customer_Zip, Customer_State, Card_Name, Credit_Card_Number, Exp_Month, Exp_Year, CVV) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+
+  $stmt->bind_param("i,s,s,s,s,s,i,s,s,s,i,i,i", $ID, $name ,$email ,$phone,$addr ,$city ,$zip ,$state,$card_name ,$card_num ,$Exp_month, $Exp_year ,$cvv );
+
+  $stmt->execute();
+
+  $stmt->close();
+  $conn->close();
+
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -243,40 +281,6 @@ footer a {
 }
 
 </style>
-<script type="text/javascript">
-
-function Insert_Customer($name ,$email ,$phone,$addr ,$city ,$zip ,$state,$card_name ,$card_num ,$Exp_month, $Exp_year ,$cvv) 
-		{
-    var ID = random.randomint(100000,999999)
-    var name = $name
-    var email = $email
-    var phone = $phone
-    var addr = $addr
-    var city = $city
-    var zip = $zip
-    var state = $state
-    var card_name = $card_name
-    var card_num = $card_num
-    var Exp_month = $Exp_month
-    var Exp_year = $Exp_year
-    var cvv = $cvv
-    
-			$.ajax({
-            	type: 'POST',
-            	url: 'InsertProduct.php',
-            	data: { ID:ID, name:name ,email:email ,phone:phone, addr:addr , city:city ,zip:zip ,state:state,card_name:card_name ,card_num:card_num ,Exp_month:Exp_month, Exp_year:Exp_year ,cvv:cvv},
-            	success: function(result) {
-                	$('#sonuc').html(result);
-            	},
-           		error: function() {
-                	alert('ERROR');
-            	}
-        	});
-
-			alert("Sucess")
-    }
-
-</script>
 
 </head>
 <body>
